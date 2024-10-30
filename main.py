76861c65fab2753bb9fee8a44d095b44882adc23
renegadelha <renegadelha@gmail.com>
2024-10-30 11:36:00 -0300

    aula3010

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import *
 app = Flask(__name__)
 
+convidados = ['tiberio','adson','risonelha','lauany','mariana','antonio','joao pedro']
+
 @app.route('/')
 def home():
     return render_template('principal.html')
@@ -8,5 +10,15 @@ def home():
 @app.route('/trabalheaqui')
 def trabalheconosco():
     return render_template('trabalheaqui.html')
+
+
+@app.route('/verificar_convite', methods=['POST'])
+def verificar_pessoa():
+    nome_pessoa = request.form.get('nome_pessoa')
+    if nome_pessoa in convidados:
+        return 'vc foi convidado'
+    else:
+        return 'nem apareça na festa'
+
 
 app.run(debug=True)
